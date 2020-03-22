9d26d357bb0ddc3f37b2b42f82a29f9e3b89c785
Rojoniaina RAJOELISOLO <rojoniaina.rajoelisolo@proximitybbdo.fr>
2020-03-23 02:59:35 +0400

    removed unusual controllers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Username from "./components/Username";
-import ListeHobbies from "./components/ListeHobbies";
-import Photos from "./components/Photos";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import TestMaterialUI from "./components/TestMaterialUI";
-import GroupeRock from "./components/GroupeRock";
 import GroupRockDetail from "./components/GroupRockDetail/GroupRockDetail";
 import Accueil from "./components/Accueil";
 
@@ -15,10 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Route exact path="/" component={GroupRockDetail}></Route>
-        <Route path="/ui" component={TestMaterialUI}></Route>
-        <Route path="/photos/:id" component={Photos}></Route>
         <Route path="/accueil" component={Accueil}></Route>
-        <Route path="/rock" component={GroupeRock} />
       </BrowserRouter>
     </div>
   );
